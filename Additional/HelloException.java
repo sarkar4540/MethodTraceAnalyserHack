4ff312299ac5f5739b8b7fad1136285e36b5c966
Aniruddha Sarkar <sarkar4540@india.com>
2019-07-14 01:19:01 +0530

    Added Aditional Files!

---
+++ Additional/HelloException.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * Sample program HelloException
+ * @author ani
+ */
+public class HelloException {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.Exception
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        
+        throw new Exception("Hello Exception!");
+    }
+    
+}
---
+++ Additional/HelloWorld.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Sample file HelloWorld
+ * @author ani
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
